76863fd9c5017ecc0566c249df45537570e5c49b
ribeirosofia <sofia.ribeiro@creditas.com>
2023-11-01 12:59:04 -0300

    feat: implement btnClear

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ const btnMin = document.getElementById("btn-min");
 const btnMultiply = document.getElementById("btn-multiply");
 const btnDiv = document.getElementById("btn-div");
 const btnDel = document.getElementById("btn-del");
+const btnClear = document.getElementById("btn-clear");
 
 btnSum.addEventListener("click", (e) => {
   e.preventDefault();
@@ -30,9 +31,18 @@ btnDel.addEventListener("click", (e) => {
   del();
 });
 
+btnClear.addEventListener("click", (e) => {
+  e.preventDefault();
+  clear();
+});
+
 const del = () => {
   const currentContent = outputDisplay.textContent;
   if (currentContent.length > 0) {
     outputDisplay.textContent = currentContent.slice(0, -1);
   }
+};
+
+const clear = () => {
+  outputDisplay.textContent = "";
 };
