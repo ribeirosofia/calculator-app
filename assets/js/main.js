b23510bf2b9bd9c85d1d496de7d8c30f73a52894
ribeirosofia <sofia.ribeiro@creditas.com>
2023-11-07 12:07:18 -0300

    feat: implement operators functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,3 +54,18 @@ const del = () => {
 const clear = () => {
   outputDisplay.textContent = "";
 };
+
+const add = (a, b) => {
+  return a + b;
+};
+const minus = (a, b) => {
+  return a - b;
+};
+const multiply = (a, b) => {
+  return a * b;
+};
+const divide = (a, b) => {
+  if (b !== "0") {
+    return a / b;
+  }
+};
